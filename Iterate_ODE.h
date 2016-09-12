42a4d2da9e26775475ef6af51d6730cff8f184da
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-09-12 15:22:54 +0200

    Use iterator based loops for iteration

--- Iterate_ODE.h
+++ Iterate_ODE.h
@@ -21,57 +21,54 @@
 */
 #ifndef ODE_H
 #define ODE_H
-#include <cmath>
 #include <vector>
 #include "Inhibitory_Neuron.h"
 #include "Pyramidal_Neuron.h"
 #include "Reticular_Neuron.h"
 #include "Thalamocortical_Neuron.h"
 
-using std::vector;
+void Iterate_ODE(std::vector<Pyramidal_Neuron>& PY,
+                 std::vector<Inhibitory_Neuron>& IN,
+                 std::vector<Thalamocortical_Neuron>& TC,
+                 std::vector<Reticular_Neuron>& RE) {
+    /* Parameters for the parallelization */
+    extern const int N_Cores;
 
-void Iterate_ODE(vector<Pyramidal_Neuron>& PY,
-				 vector<Inhibitory_Neuron>& IN,
-				 vector<Thalamocortical_Neuron>& TC,
-				 vector<Reticular_Neuron>& RE) {
-	/* Parameters for the parallelization */
-	extern const int N_Cores;
+    /* First get all the RK terms */
+    for (unsigned i=0; i < 4; i++) {
+        #pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
+        for(auto it = PY.begin(); it < PY.end(); ++it)
+            it->set_RK(i);
 
-	/* First get all the RK terms */
-	for (int i=0; i < 4; i++) {
-		#pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
-		for(unsigned j=0; j < PY.size(); j++)
-			PY[j].set_RK(i);
+        #pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
+        for(auto it = IN.begin(); it < IN.end(); ++it)
+            it->set_RK(i);
 
-		#pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
-		for(unsigned j=0; j < IN.size(); j++)
-			IN[j].set_RK(i);
+        #pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
+        for(auto it = TC.begin(); it < TC.end(); ++it)
+            it->set_RK(i);
 
-		#pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
-		for(unsigned j=0; j < TC.size(); j++)
-			TC[j].set_RK(i);
+        #pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
+        for(auto it = RE.begin(); it < RE.end(); ++it)
+            it->set_RK(i);
+    }
 
-		#pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
-		for(unsigned j=0; j < RE.size(); j++)
-			RE[j].set_RK(i);
-	}
+    /* Add the RK terms up*/
+    #pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
+    for(auto it = PY.begin(); it < PY.end(); ++it)
+        it->add_RK();
 
-	/* Add the RK terms up*/
-	#pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
-	for(unsigned j=0; j < PY.size(); j++)
-		PY[j].add_RK();
+    #pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
+    for(auto it = IN.begin(); it < IN.end(); ++it)
+        it->add_RK();
 
-	#pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
-	for(unsigned j=0; j < IN.size(); j++)
-		IN[j].add_RK();
+    #pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
+    for(auto it = TC.begin(); it < TC.end(); ++it)
+        it->add_RK();
 
-	#pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
-	for(unsigned j=0; j < TC.size(); j++)
-		TC[j].add_RK();
-
-	#pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
-	for(unsigned j=0; j < RE.size(); j++)
-		RE[j].add_RK();
+    #pragma omp parallel for num_threads(N_Cores) schedule(dynamic)
+    for(auto it = RE.begin(); it < RE.end(); ++it)
+        it->add_RK();
 }
 
 #endif // ODE_H
